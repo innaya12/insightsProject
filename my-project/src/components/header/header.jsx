de627bb181c4417ef6caf701de21cfbb1ad1559b
innaya12 <yakubov.inna2@gmail.com>
2020-10-08 15:52:13 +0300

    responsive header done

---
+++ my-project/src/components/header/header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './style.css';
+import Logo from '../../assets/headerImg.png';
+
+class Header extends React.Component{
+
+    render(){
+        return (
+            <div className={"header-wrapper container-fluid"}>
+                <div className={"d-none d-lg-block desktop justify-content-center"}>
+                    <h5>לגבש אסטרטגיה</h5>
+                </div>
+                <div className={"d-block d-lg-none mobile"}>
+                    <img src={Logo} alt={"headerImg"} width={"55px"}></img>
+                </div>
+            </div>
+        )
+    }
+};
+
+export default Header;
